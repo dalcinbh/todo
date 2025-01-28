60418b7dcc52253b17f209e6010cd6708cc9952e
Adriano Alves Dal Cin Costa <adriano@agiliza.top>
2025-01-28 07:42:38 -0300

    my first commit for todo list

---
+++ lambda/src/handlers/createTask.ts
@@ -0,0 +1,22 @@
+import { DynamoDB } from 'aws-sdk';
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export const handler = async (event: any) => {
+  const { title, description, dueDate } = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.TASKS_TABLE,
+    Item: {
+      taskId: Date.now().toString(),
+      title,
+      description,
+      dueDate,
+      completed: false,
+    },
+  };
+
+  await dynamoDb.put(params).promise();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Tarefa criada com sucesso!' }),
+  };
+};
